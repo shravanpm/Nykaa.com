a0cba1a7e0403449a9140477be84b05c901315bb
Pankaj Kandpal <97446286+pankajnandi@users.noreply.github.com>
2022-05-04 10:20:30 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,1 @@
-import { useState } from 'react'
 
-import './App.css'
-import { ProductDetails } from "./Components/ProductDetails"
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <ProductDetails></ProductDetails>
-    </div>
-  )
-}
-
-export default App
